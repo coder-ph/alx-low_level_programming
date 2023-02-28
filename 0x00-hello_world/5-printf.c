c4dd86bdd2afa32f7de8e1bf62252c52e17fe2af
coder-ph <phelixmbani@gmail.com>
2023-02-28 19:26:45 +0300

    task 5 using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - a program for printing a line using printf function
+ *return: o
+ */
+int main (void){
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
+
