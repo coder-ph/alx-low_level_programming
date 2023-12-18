fed41d14c2257ab71a1f7e9dd89fc8cee0edc9fa
Phelix Mbani <phelixmbani@gmail.com>
2023-12-17 23:40:07 -0800

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unixstd.h>
+/**
+ * main - a c program to print a line to the std err
+ * Return 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
