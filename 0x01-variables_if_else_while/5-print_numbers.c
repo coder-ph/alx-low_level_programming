0fcf9072c1d0f51354e678ea5f844ed380b00ee3
Phelix Mbani <phelixmbani@gmail.com>
2023-12-19 13:25:48 -0800

    printin numbers from 0-10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point of our program
+ * Return: 0, success
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
