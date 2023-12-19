b494fe07b007ddf7bcc9c6a8c143e4c15ca31e1a
Phelix Mbani <phelixmbani@gmail.com>
2023-12-19 13:45:56 -0800

    printing z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point of our program
+ * Return: 0, success
+*/
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c>= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
