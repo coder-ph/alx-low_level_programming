df63beaa96082533f361742e7c79701645ddeebe
Phelix Mbani <phelixmbani@gmail.com>
2023-12-19 12:16:48 -0800

    a program that prints the alphabet in lowecase using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - this is the start of ou program
+ * Return: 0, success
+*/
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
