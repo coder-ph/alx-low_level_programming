0aad3592872d2f964aa70992caa46aed28710840
Phelix Mbani <phelixmbani@gmail.com>
2023-12-28 01:52:59 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _islower - a function for checking lower characters
+* main: program entry point
+* @c: parameter to be checked
+* return: 0 or 1
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
