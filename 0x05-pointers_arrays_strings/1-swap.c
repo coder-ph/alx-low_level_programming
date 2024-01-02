0e0994bd6d5d61a6f92e8e4fe9dd2a88e7b3cc7f
Phelix Mbani <phelixmbani@gmail.com>
2024-01-02 12:35:49 -0800

    dont swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+incude "main.h"
+/**
+* swap_int - swap two values
+* @a: first value
+* @b: second value
+* Return: void
+*/
+void swap_int(int *a, int *b);
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
