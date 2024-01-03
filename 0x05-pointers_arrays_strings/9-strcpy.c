03a287d9a0308a22792724024ec3214d06a3982d
Phelix Mbani <phelixmbani@gmail.com>
2024-01-03 01:44:33 -0800

    fyi

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - function for copying strings
+ *@dest: destination
+ *@src: source
+ *Return: o success
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
